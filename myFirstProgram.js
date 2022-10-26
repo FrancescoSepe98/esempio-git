43db11b422f77c99b594c61ba0cd4d1767cae5c4
FrancescoSepe98 <sepe.francesco.lavoro@gmail.com>
2022-10-26 20:33:26 +0200

    creation of my first program

---
+++ myFirstProgram.js
@@ -0,0 +1,15 @@
+let i;
+let j=0;
+let sum = 0;
+let newSum =0;
+for (i=0;i<=10;i++){
+    sum = sum+i; 
+    console.log(i);
+}
+
+
+while(j<=10){
+    newSum= newSum+j;
+    j++;
+    };
+console.log(newSum);
